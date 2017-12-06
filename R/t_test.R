74a157757bf5e3fc131f619ef029696fcfba12d7
Jieun Lee <jelee626@gmail.com>
2017-12-06 13:26:25 -0700

    ttest package upload

---
+++ R/t_test.R
@@ -0,0 +1,28 @@
+#' Format T-Test
+#'
+#' Formats a t-test object to look gorgeous.
+#'
+#' @param fit a t-test object
+#' @param rounding the number of decimal places for the output
+#'
+#' @return a data.frame with useful t-test information
+#'
+#' @export
+
+format_t = function(fit, rounding = 3){
+
+  output = data.frame(
+    "T" = fit$statistic,
+    "DF" = fit$parameter,
+    "P-Value" = fit$p.value,
+    "Mean Difference" = fit$estimate[1] - fit$estimate[2],
+    "Lower" = fit$conf.int[1],
+    "Upper" = fit$conf.int[2],
+    row.names = NULL
+  )
+
+  output[] = lapply(output, round, rounding)
+  output
+}
+
+`%>%` = magrittr::`%>%`
